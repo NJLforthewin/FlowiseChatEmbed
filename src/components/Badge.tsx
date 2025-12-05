b0b2d0bf467dc64d6025572f2063153d48c99132
NJLforthewin <johnchristiansancija0@gmail.com>
2025-12-05 23:01:30 +0800

    tried

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -11,28 +11,7 @@ type Props = {
 const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
-  let liteBadge: HTMLAnchorElement | undefined;
   let observer: MutationObserver | undefined;
-
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
-
-  onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
-  });
 
   onCleanup(() => {
     if (observer) observer.disconnect();
